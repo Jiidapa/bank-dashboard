b1cf58045b9dfb3fd8bd89f41683e693bc1b827b
jiidapa <eye.jiida@gmail.com>
2025-02-13 22:11:08 +0700

    style: Format code using prettier

--- src/components/Splash/Splash.stories.tsx
+++ src/components/Splash/Splash.stories.tsx
@@ -1,15 +1,15 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from "@storybook/react";
 
-import Splash from './Splash';
+import Splash from "./Splash";
 
 const meta: Meta<typeof Splash> = {
-    title: "Components/Splash",
-    component: Splash,
+  title: "Components/Splash",
+  component: Splash,
 };
 
 export default meta;
 type Story = StoryObj<typeof Splash>;
 
 export const Default: Story = {
-    args: {},
+  args: {},
 };
